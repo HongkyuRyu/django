3663aea931c4fca82e097c04577b708241f4312f
HongkyuRyu <69923886+HongkyuRyu@users.noreply.github.com>
2023-08-02 17:51:25 +0900

    chore: 암호화

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.hashers import make_password
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password=None):
         if not email:
             raise ValueError("이메일은 필수로 입력해야합니다.")
         user = self.model(email=self.normalize_email(email))
-        user.set_password(password)
+        user.password = make_password(password)
         user.save()
         return user
     
@@ -14,4 +15,5 @@ class CustomUser(AbstractBaseUser):
     email = models.EmailField(unique=True)
     objects = CustomUserManager()
     USERNAME_FIELD = "email"
+    
     
